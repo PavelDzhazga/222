62865c3c6dd5a2e236b7b0c0f8c4740fa33f7561
Pavel Dzhazga <dzhazga2002@mail.ru>
2023-02-15 19:39:06 +0300

    php

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 public class Egg6 {
 
     private final Needle7 needle7;
+
     public Egg6(Needle7 needle7) {
         this.needle7 = needle7;
     }

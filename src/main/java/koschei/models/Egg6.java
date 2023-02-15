9574692526cbe076212c819724699a8e123dee12
Pavel Dzhazga <dzhazga2002@mail.ru>
2023-02-15 16:11:50 +0300

    php

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,13 +1,11 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
 
-    private Needle7 needle7;
-    @Autowired
+    private final Needle7 needle7;
     public Egg6(Needle7 needle7) {
         this.needle7 = needle7;
     }
